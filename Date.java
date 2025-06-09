c65ef3a2514ae7143713204d5ca95791efb19f33
chetanbca102-4 <01fe23bca102@kletech.ac.in>
2025-06-09 06:13:32 +0530

    Update Date.java

--- Date.java
+++ Date.java
@@ -1,20 +1,20 @@
-class Student {
+public class Student {
     String name;
     int age;
     String DOB;
 
     Student(String name, int age, String DOB){
-        this.name=name;
-        this.age=age;
-        this.DOB=DOB;
+        this.name = name;
+        this.age = age;
+        this.DOB = DOB;
     }
-    void display(){
-        System.out.println("Student Name:"+name+" Student age:"+age+" Student DOB:"+DOB);
+
+    void display() {
+        System.out.println("Student Name: " + name + ", Student Age: " + age + ", Student DOB: " + DOB);
     }
-}
-public class Date {
+
     public static void main(String[] args) {
-        Student s1= new Student("Lalu",20,"03-02-2005");
+        Student s1 = new Student("Lalu", 20, "03-02-2005");
         s1.display();
     }
 }
